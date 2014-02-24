fda460e61237ac34f0def3d63cb7fd74f398f740
周梦康 <i@zhoumengkang.com>
2014-02-24 22:10:54 +0800

    Update svn.php

--- svn.php
+++ svn.php
@@ -3,8 +3,9 @@
  * svn钩子post-commit里执行的文件
  * 存放在svn服务器上
  */
+$updateUrl = "http://zhoumengkang.com/update.php";//远程测试服务器上的update.php的地址
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://zhoumengkang.com/update.php");
+curl_setopt($ch, CURLOPT_URL, $updateUrl);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_exec($ch);
 curl_close($ch);
