7efb11e67a3bbc6b2eb5b50e892f741948378a8e
周梦康 <i@zhoumengkang.com>
2014-03-16 09:05:32 +0800

    如果在$read中可以匹配到“error/conflict”，就应该发送邮件到管理员的邮箱了

--- update.php
+++ update.php
@@ -9,7 +9,8 @@ error_reporting(E_ALL);
 //"/home/wwwroot/test/" 为代码更新到的指定目录路径
 
 $handle = popen('svn up --username zmk --password 123456 /home/wwwroot/test/ 2>&1','r');
-echo "'$handle'; " . gettype($handle) . "\n";
-$read = fread($handle, 2096);
+//echo "'$handle'; " . gettype($handle) . "\n";
+$read = stream_get_contents($handle);
+//TODO 如果在$read中可以匹配到“error/conflict”，就应该发送邮件到管理员的邮箱了！
 echo $read;
 pclose($handle);
